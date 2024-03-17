fe82b872902f0ef228cebf626fc52fc8a8ab9af1
Kerie <ogedegbekerieo@gmail.com>
2024-03-17 23:21:14 +0100

    secs

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
-
+// Set interval
 setInterval(() => {
   let currentTime = new Date();
   hrs.innerHTML =
